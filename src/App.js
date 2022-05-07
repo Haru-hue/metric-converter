eb83942b8afaeb4e95064ac0ab370e0ceee61dab
Haru-hue <53390409+Haru-hue@users.noreply.github.com>
2022-05-07 07:22:03 +0100

    Update to App.js and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [unit, setUnit] = useState(0)
+
+  const handleChange = (e) => {
+    setUnit(e.target.value)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='title'>
+        <h3>Metric/Imperial unit conversion</h3>
+        <input type="text" placeholder='0' onChange={handleChange}></input>
+      </section>
+      <section className='conversion'>
+          <div className='converter'>
+              <h4><b>Length (Meter/Feet)</b></h4>
+              <div className='row'>
+                  <span>{unit} meters = {(unit*3.281).toFixed(3)} feet | {unit} feet = {(unit/3.281).toFixed(3)} meters</span>
+              </div>
+          </div>
+          <div className='converter'>
+            <h4><b>Volume (Liters/Gallons)</b></h4>
+              <div className='row'>
+                  <span>{unit} liters = {(unit/3.785).toFixed(3)} gallon | {unit} gallon = {(unit*3.785).toFixed(3)} liters</span>
+              </div>
+          </div>
+          <div className='converter'>
+            <h4><b>Mass (Kilograms/Pounds)</b></h4>
+              <div className='row'>
+                  <span>{unit} kilogram = {(unit*2.205).toFixed(3)} pound | {unit} pounds = {(unit/2.205).toFixed(3)} kilogram</span>
+              </div>
+          </div>
+      </section>
+    </main>
   );
 }
 
